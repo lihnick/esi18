773f2a09a592e4a07a62e4543ce6f88fc6368412
Nick Li <hli109@hawk.iit.edu>
2018-07-12 13:54:44 -0500

    update

--- simple/SimpleShip.java
+++ simple/SimpleShip.java
@@ -24,35 +24,11 @@ public class SimpleShip extends Ship {
      */
     @Override
     protected void doTurn(Arena arena) {
-        // get a random integer between 0 (inclusive) and 4 (exclusive)
-        int randomMovement = arena.getRandom().nextInt(4);
-
-        // randomMovement ranges from 0-3, which corresponds to the 4 possible direction of movement
-        if (randomMovement == 0) {
-            this.move(arena, Direction.NORTH);
-        }
-        else if (randomMovement == 1) {
-            this.move(arena, Direction.SOUTH);
-        }
-        else if (randomMovement == 2) {
-            this.move(arena, Direction.WEST);
-        }
-        else if (randomMovement == 3) {
-            this.move(arena, Direction.EAST);
-        }
-
-        // Get a list of nearby ships, ships that are within this ship's range
-        // If there's no nearby ship, this list will be size 0
-        List<Ship> nearby = this.getNearbyShips(arena);
-        
-        for (int i = 0; i < nearby.size(); i++) {
-            Ship target = nearby.get(i);
-            Coord location = target.getCoord();
-            int x = location.getX();
-            int y = location.getY();
-            this.fire(arena, x, y);
-        }
-        
+        this.move(arena, Direction.EAST);
+        Coord location = this.getCoord();
+        int x = location.getX();
+        int y = location.getY();
+        this.fire(arena, x+1, y);
     }
     
 }
