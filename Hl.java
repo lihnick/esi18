d68e0f5e97388b2c34112846bffa6dbbc7ec15e0
Nick Li <hli109@hawk.iit.edu>
2018-07-24 18:57:45 -0500

    added filters

--- Hl.java
+++ Hl.java
@@ -18,6 +18,14 @@ public class Hl extends Ship {
         this.initializeSpeed(1);
         this.initializeRange(4);
     }
+
+    @Override
+    public boolean equals(Ship other) {
+        if (this instanceof other) {
+            return true;
+        }
+        return false;
+    }
     
     /*
         Goal: save information about each ship where they compose into a single action
@@ -50,10 +58,16 @@ public class Hl extends Ship {
             int friendly = (this.isSameTeamAs(ship)) ? -1 : 1;
             // account for potential dangers from ship's movement then attack combo
             int multiplier = ship.getSpeed(); 
-            
-            for (int mapX = 0; mapX < heatmap.length; mapX++) {
 
-                for (int mapY = 0; mapY < heatmap[0].length; mapY++) {
+            // reduce the number of heatmap that needs to be traverse
+            int minX = Math.max(0, ship.getX() - (ship.getRange() + ship.getSpeed()));
+            int minY = Math.max(0, ship.getY() - (ship.getRange() + ship.getSpeed()));
+            int maxX = Math.min(arena.getX(), ship.getX() + ship.getRange() + ship.getSpeed());
+            int maxY = Math.min(arena.getY(), ship.getY() + ship.getRange() + ship.getSpeed());
+            
+            for (int mapX = minX; mapX < maxX; mapX++) {
+
+                for (int mapY = minY; mapY < maxY; mapY++) {
 
                     int xdiff = Math.abs(mapX - ship.getX());
                     int ydiff = Math.abs(mapY - ship.getY());
@@ -76,23 +90,80 @@ public class Hl extends Ship {
 
                 }
             }
+            result.put(ship, heatmap);
         }
+        return result;
     }
 
-    private LinkedHashMap<Ship, int[][]> filterNearby(Arena arena) {
+    private LinkedHashMap<Ship, int[][]> filterNearby(LinkedHashMap<Ship, int[][]> data) {
+        LinkedHashMap<Ship, int[][]> result = new LinkedHashMap<>();
 
+        result = data.entrySet().stream().filter((ship) -> {
+            if (Math.abs(this.getX() - ship.getX()) <= this.getRange() && 
+                Math.abs(this.getY() - ship.getY()) <= this.getRange() ) {
+                return true;
+            }
+            return false;
+        }).collect(Collectors.toMap(ship -> ship.getKey(), ship -> ship.getValue()));
+
+        return result;
     }
 
-    private LinkedHashMap<Ship, int[][]> filterSinkable(Arena arena, boolean sinkable) {
+    private LinkedHashMap<Ship, int[][]> filterSinkable(LinkedHashMap<Ship, int[][]> data, boolean sinkable) {
+        LinkedHashMap<Ship, int[][]> result = new LinkedHashMap<>();
 
+        result = data.entrySet().stream().filter((ship) -> {
+            if (ship.getHealth() <= this.getRemainingShots()) {
+                return (true == sinkable);
+            }
+            return (false == sinkable);
+        }).collect(Collectors.toMap(ship -> ship.getKey(), ship -> ship.getValue()));
+
+        return result;
     }
 
-    private LinkedHashMap<Ship, int[][]> filterFriendly(Arena arena, boolean foe) {
+    private LinkedHashMap<Ship, int[][]> filterFriendly(LinkedHashMap<Ship, int[][]> data, boolean foe) {
+        LinkedHashMap<Ship, int[][]> result = new LinkedHashMap<>();
 
+        result = data.entrySet().stream().filter((ship) -> {
+            // filter out your own ship in all cases
+            if (!this.equals(ship)) {
+                if (!this.isSameTeamAs(ship) == foe) {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }).collect(Collectors.toMap(ship -> ship.getKey(), ship -> ship.getValue()));
+
+        return result;
     }
 
     private LinkedHashMap<Ship, int[][]> filterPriority(Arena arena) {
+        LinkedHashMap<Ship, int[][]> result = new LinkedHashMap<>();
 
+        result = data.entrySet().stream().sorted((lhs, rhs) -> {
+            // sort by highest firepower/health ratio first
+            double rhsRatio = (double) rhs.getFirepower()/rhs.getHealth();
+            double lhsRatio = (double) lhs.getFirepower()/lhs.getHealth();
+
+            if ((int)rhsRatio != (int)lhsRatio) {
+                return (int)(rhsRatio - lhsRatio);
+            }
+            // sort by the highest range first
+            else if (rhs.getRange() != lhs.getRange()) {
+                return rhs.getRange() - lhs.getRange();
+            }
+            // sort by lowest health first
+            else if (lhs.getHealth() != rhs.getHealth()) {
+                return lhs.getHealth() - rhs.getHealth();
+            }
+            else {
+                return lhs.getName().compareTo(rhs.getName());
+            }
+        }).collect(Collectors.toMap(ship -> ship.getKey(), ship -> ship.getValue()));
+
+        return result;
     }
 
     private int[][] composeMap(Arena arena) {
